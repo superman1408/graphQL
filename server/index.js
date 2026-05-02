c7e04df7b81e50a61ecdaeaa9c1a740aed199b9b
superman1408 <superman14082017@gmail.com>
2026-05-02 18:13:56 +0530

    adding comment to the codes

--- server/index.js
+++ server/index.js
@@ -11,14 +11,18 @@ import { expressMiddleware } from '@apollo/server/express4';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+// middleware setup for CORS and body parsing
 app.use(cors());
 app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to the GraphQL API server!');
 });
 
 
+// Server is running at http://localhost:4000
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
