d23f37b3739cdeaf6f4853c89e83256619fceda2
superman1408 <superman14082017@gmail.com>
2026-05-05 17:51:22 +0530

    backend for apollo server is working fine.

--- server/resolvers/resolvers.js
+++ server/resolvers/resolvers.js
@@ -1,5 +1,30 @@
+import { TODOS } from '../db/todos.js';
+import { USERS } from '../db/users.js';
+
+
+
 export const RESOLVERS = {
   Query: {
-    hello: () => 'Hello, world!',
+    getTodos: () => TODOS,
+    getUsers: () => USERS,
+    getUser: (parent, { id }) => {//linear search for user by id
+      return USERS.find(user => user.id === parseInt(id));
+    },
+    getTodoByUserId: (parent, { id }) => {
+      return TODOS.filter(todo => todo.userId === parseInt(id));
+    },
   },
+
+  TODO: {
+    user: (todos) => {
+      return USERS.find(user => user.id === todos.userId);
+    }
+  },
+
+
+  User: {
+    todos: (user) => {
+      return TODOS.filter(todo => todo.userId === user.id);
+    }
+  }
 };
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,23 @@
 export const TYPEDEFS = `
-  type Query {
-    hello: String
-  }
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        todos: [TODO]
+    }
+
+    type TODO {
+        id: ID!
+        title: String!
+        description: String
+        completed: Boolean!
+        user: User
+    }
+
+    type Query {
+        getTodos: [TODO]
+        getUsers: [User]
+        getUser(id: ID!): User
+        getTodoByUserId(id: ID!): [TODO]
+    }
 `;
